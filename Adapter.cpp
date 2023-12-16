ca54eacdaf6bde33d64de03675059444b462d1eb
hma-3 <maria.gula0307@gmail.com>
2023-12-17 01:34:25 +0200

    Initial commit

---
+++ Adapter.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Circle {
+public:
+    Circle() {}
+    Circle(float radius) : radius_(radius) {}
+
+    float getRadius() const { return radius_; }
+
+private:
+    float radius_;
+};
+
+class SquareCircleAdapter : public Circle {
+public:
+    SquareCircleAdapter(float side_length) : side_length_(side_length) {}
+
+    float getRadius() const {
+        return side_length_ / sqrt(2.0f);
+    }
+
+private:
+    float side_length_;
+};
+
+int main() {
+    SquareCircleAdapter adapter(10.0f);
+
+    cout << "The radius of the adapted circle is " << adapter.getRadius() << endl;
+
+    return 0;
+}
